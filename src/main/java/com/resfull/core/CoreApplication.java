0f4a7215b275c8b546a3406e200bf195555d9906
kbaez <karina.baez@dinmax.com>
2018-10-26 13:01:14 -0300

    new database

--- src/main/java/com/resfull/core/CoreApplication.java
+++ src/main/java/com/resfull/core/CoreApplication.java
@@ -1,7 +1,9 @@
 package com.resfull.core;
 
+import com.resfull.core.controller.ClienteRest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class CoreApplication {
