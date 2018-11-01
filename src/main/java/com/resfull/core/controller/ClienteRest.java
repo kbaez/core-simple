bd92fefd5196e8c542bb5ce73384d79603d290eb
kbaez <karina.baez@dinmax.com>
2018-11-01 11:27:37 -0300

    change view

--- src/main/java/com/resfull/core/controller/ClienteRest.java
+++ src/main/java/com/resfull/core/controller/ClienteRest.java
@@ -5,19 +5,42 @@
  */
 package com.resfull.core.controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import com.resfull.core.entity.Nota;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/nota")
-@EnableAutoConfiguration
 public class ClienteRest {
 
+    private final String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkYXZpZGJyaW9uZXMifQ.74o-HxtdCtSv4HD6ADn7TgLv2qUDdESR5J_kk8FdGTo43psguoL6Pi37ktuPlJSrRXhkUFIQvxaN0pX4cLvsww";
+
     @GetMapping("/all")
-    public String devolvertodos(){
-        return "template";
+    public ModelAndView devolvertodos(){
+        ModelAndView mav = new ModelAndView("template");
+
+        RestTemplate rest = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Authorization", token);
+
+        HttpEntity entity = new HttpEntity(headers);
+
+        ResponseEntity<Nota[]> notasEntity = rest.exchange("http://localhost:8090/v1/notas",HttpMethod.GET,entity, Nota[].class);
+
+        Nota[] notas = notasEntity.getBody();
+
+        mav.addObject("notas",notas);
+        return mav;
     }
-    
+
 }
+
